c8cb3c38b0a45c16e68fe507dc5b59423b4e6f58
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2024-07-03 17:34:59 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#### `setup.py`
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name='ms_signal_analysis',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'tqdm',
+        'ray',
+    ],
+    author='jurikri',
+    author_email='jurikri@gmail..com',
+    description='A package to perform signal analysis using Morlet wavelet transforms on EEG data',
+    url='https://github.com/jurikri/morlet_wavelet',
+)
