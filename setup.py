a4080037d7e791f29b4c5a24af476f4694ef1d84
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2024-07-03 17:46:14 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ms_signal_analysis',
+    name='msanalysis',
     version='0.1',
     packages=find_packages(),
     install_requires=[
