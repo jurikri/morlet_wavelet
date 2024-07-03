8a533f0410514b81fd3063d1c805bd47ea2d017d
Myeong Seong Bak <41930943+jurikri@users.noreply.github.com>
2024-07-03 18:00:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='msanalysis',
     version='0.1',
-    packages=find_packages(),
+    packages=['morlet_wavelet'],
     install_requires=[
         'numpy',
         'scipy',
